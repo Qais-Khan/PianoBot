241738b5e1d3c60060ec46078a498f927b8a295a
Qais <shafarnaz@gmail.com>
2021-05-29 16:40:25 -0700

    Starting code (No real code)

---
+++ PianoBot.py
@@ -0,0 +1,33 @@
+#Imports
+import pyautogui
+import time
+
+
+#Getting mouse position
+# This will let you "measure" the coordinates of specific points on screen by hovering your mouse over it
+# Very useful - you'll see why when I talk about the design process
+def get_mouse_pos():
+    while True:
+        print(pyautogui.position())
+        time.sleep(1)
+
+
+
+
+
+
+
+# ============================
+#          MAIN BODY
+# ============================
+# This is where the actual code for the bot will be written
+# Don't worry, it's not complicated, and I'll walk you through it in the workshop anyway :)
+#while True
+
+    # COLUMN 1
+
+    # COLUMN 2
+
+    # COLUMN 3
+
+    # COLUMN 4
